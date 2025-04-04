d4af7b99b3d5ed9d7a917ea22e64be0b617aaf60
alysaleong <leong.alysa@gmail.com>
2025-04-04 18:32:39 -0400

    implemented cov matrix

--- backend/documentation/portfolio.md
+++ backend/documentation/portfolio.md
@@ -23,7 +23,7 @@
 ```
 
 ### Display stock holdings of a given portfolio
-#### req: GET http://localhost:4000/api/portfolios/:pid
+#### req: GET http://localhost:4000/api/portfolios/pid/:pid
 #### res:
 ```
 [
@@ -59,6 +59,36 @@
 #### req: POST http://localhost:4000/api/portfolios/deposit
 #### res: `{message: `\$${deposit} deposited`}`
 
+### Withdraw from cash account
+#### req: POST http://localhost:4000/api/portfolios/withdraw
+#### res: `{message: `\$${withdrawal} withdrew`}`
+
+### Display stocks bought
+#### req: GET http://localhost:4000/api/portfolios/bought/:pid
+#### res: 
+```
+[
+    {
+        "symbol": "POO",
+        "timestamp": "2025-03-31T04:00:00.000Z",
+        "total_value": "420.69"
+    }
+]
+```
+
+### Display stocks sold
+#### req: GET http://localhost:4000/api/portfolios/sold/:pid
+#### res:
+```
+[
+    {
+        "symbol": "POO",
+        "timestamp": "2025-03-31T04:00:00.000Z",
+        "total_value": "420.69"
+    }
+]
+```
+
 ### Purchase a stock of a given portfolio, stock symbol, and quantity
 #### req: POST http://localhost:4000/api/portfolios/buy
 #### res: `{message: `${symbol} purchased for \$${total_price}`}`
@@ -66,3 +96,35 @@
 ### Sell a stock of a given portfolio, stock symbol, and quantity
 #### req: POST http://localhost:4000/api/portfolios/sell
 #### res: `{message: `${symbol} sold for \$${total_price}`}`
+
+### Compute covariance matrix for the stocks of a given portfolio and time interval
+#### req: POST http://localhost:4000/api/portfolios/cov
+#### res: 
+```
+[
+    [
+        [
+            1.8471096947738253,
+            "PFE",
+            "PFE"
+        ],
+        [
+            -2.1891395222146528e-13,
+            "PFE",
+            "LEG"
+        ]
+    ],
+    [
+        [
+            -2.1891395222146528e-13,
+            "LEG",
+            "PFE"
+        ],
+        [
+            1.861504743083004,
+            "LEG",
+            "LEG"
+        ]
+    ]
+]
+```
--- backend/services/stockServices.js
+++ backend/services/stockServices.js
@@ -1,6 +1,6 @@
 import { pool } from '../server.js'
 
-export { formatDate, isValidDate }
+export { formatDate, isValidDate, computeCov }
 
 // reformat today's date
 function formatDate() {
@@ -24,4 +24,83 @@ function isValidDate(dateStr) {
       date.getMonth() === month - 1 &&
       date.getDate() === day
     );
+};
+
+// compute the covariance of the given stocks for the given time interval
+async function computeCov(symbol1, symbol2, start_date, end_date) {
+  // check if stocks are in historical records
+    if (symbol1 == symbol2) {
+        const stocks = await pool.query(
+          `SELECT symbol
+          FROM hist_stock_data
+          WHERE symbol = $1`,
+          [symbol1]
+        );
+        if (stocks.rows.length === 2) {
+          return null;
+        };
+    } else {
+        const stocks = await pool.query(
+          `SELECT symbol
+          FROM hist_stock_data
+          WHERE symbol IN ($1, $2)
+          GROUP BY symbol`,
+          [symbol1, symbol2]
+        );
+        if (stocks.rows.length < 2) {
+            return null;
+        };
+    };
+    
+
+    let cov = null;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+
+        // check if requested data is cached
+        const request = await client.query(
+            `SELECT cov
+            FROM cov_mat_cache
+            WHERE ((symbol1 = $1 AND symbol2 = $4) OR (symbol1 = $4 AND symbol2 = $1)) AND start_date = $2 AND end_date = $3`,
+            [symbol1, start_date, end_date, symbol2]
+        );
+
+        if (request.rows.length === 0) {
+            if (symbol1 == symbol2) {
+                cov = await client.query(
+                  `SELECT var_samp(close) AS cov
+                  FROM hist_stock_data
+                  WHERE symbol = $1 AND timestamp >= $2 AND timestamp <= $3`,
+                  [symbol1, start_date, end_date]
+                );
+            } else {
+                cov = await client.query(
+                  `SELECT covar_samp(sym1.close, sym2.close) AS cov
+                  FROM hist_stock_data sym1, hist_stock_data sym2
+                  WHERE sym1.symbol = $1 AND sym1.timestamp >= $2 AND sym1.timestamp <= $3
+                  AND sym2.symbol = $4 AND sym2.timestamp >= $2 AND sym2.timestamp <= $3`,
+                  [symbol1, start_date, end_date, symbol2]
+                );
+            }
+            
+            cov = cov.rows.map(row => row.cov)[0];
+
+            await client.query(
+              `INSERT INTO cov_mat_cache (symbol1, symbol2, cov, start_date, end_date) VALUES ($1, $5, $2, $3, $4)`,
+              [symbol1, cov, start_date, end_date, symbol2]
+            );
+        } else {
+            cov = request.rows.map(row => row.cov)[0];
+        };
+
+        await client.query("COMMIT");
+        } catch (error) {
+            await client.query("ROLLBACK");
+            client.release();
+            return null;
+        } 
+
+        client.release();
+        return Number(cov);
 };
